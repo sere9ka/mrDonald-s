891bb21f70678d35ca3c575c2a849a2c65a46221
Sergey Kovylov <sere99ka@gmail.com>
2022-05-30 14:21:08 +0300

    fix preloader

--- src/components/system-component/preloader.js
+++ src/components/system-component/preloader.js
@@ -7,8 +7,8 @@ export const Preloader = ({ customText }) => {
      
             <svg width="100%"  viewBox="0 0 276 276" fill="none" xmlns="http://www.w3.org/2000/svg" >
                 <g id="spinner">
-                    <circle id="bottom" cx="138" cy="138" r="114" stroke="#DBDBDB" stroke-width="18" />
-                    <circle id="upper" cx="138" cy="138" r="123" stroke="#72BBFF" stroke-width="30" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="373 100" style={{animationDuration:20+"s"}}/>
+                    <circle id="bottom" cx="138" cy="138" r="114" stroke="#DBDBDB" strokeWidth="18" />
+                    <circle id="upper" cx="138" cy="138" r="123" stroke="#72BBFF" strokeWidth="30" strokeLinecap="round" strokeLinejoin="round" strokeDasharray="373 100" style={{animationDuration:20+"s"}}/>
                 </g>
             </svg>
             <p>{customText}</p>
